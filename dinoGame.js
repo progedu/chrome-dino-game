b4e0143bd33f259ddf24b77e7efe270b6078aaa9
DDRAGON <davide.0829@gmail.com>
2021-07-14 13:39:48 +0900

    part3

--- dinoGame.js
+++ dinoGame.js
@@ -32,7 +32,55 @@ function init() {
     game.enemys     = [];
     game.isGameOver = false;
     game.score      = 0;
-    ctx.drawImage(game.image.bird, 500, 320);
-    ctx.drawImage(game.image.cactus, 300, 320);
-    ctx.drawImage(game.image.dino, 100, 320);
+    createDino();
+    game.timer = setInterval(ticker, 30);
 }
+
+function ticker() {
+    // 画面クリア
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // TODO 敵キャラクターの生成
+
+    // キャクター移動
+    movedino(); // 恐竜の移動
+
+    //描画
+    drawdino();// 恐竜の描画
+
+    // TODO あたり判定
+
+    // カウンターの更新
+    game.counter = (game.counter + 1) % 1000000;
+}
+
+function createDino() {
+    game.dino = {
+        x: game.image.dino.width / 2,
+        y: canvas.height - game.image.dino.height / 2,
+        moveY: 0,
+        width: game.image.dino.width,
+        height: game.image.dino.height,
+        image: game.image.dino
+    }
+}
+
+function movedino() {
+    game.dino.y += game.dino.moveY;
+    if (game.dino.y >= canvas.height - game.dino.height / 2) {
+        game.dino.y = canvas.height - game.dino.height / 2;
+        game.dino.moveY = 0;
+    } else {
+        game.dino.moveY += 3;
+    }
+}
+
+function drawdino() {
+    ctx.drawImage(game.image.dino, game.dino.x - game.dino.width / 2, game.dino.y - game.dino.height / 2);
+}
+
+document.onkeydown = function(e) {
+    if(e.key === ' ' && game.dino.moveY === 0) {
+        game.dino.moveY = -41;
+    }
+};
