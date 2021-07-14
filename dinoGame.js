f2a95e92de63d08b2fe2635b81c1113f91fd5564
DDRAGON <davide.0829@gmail.com>
2021-07-14 13:56:49 +0900

    part4

--- dinoGame.js
+++ dinoGame.js
@@ -40,13 +40,21 @@ function ticker() {
     // 画面クリア
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // TODO 敵キャラクターの生成
+    // 敵キャラクターの生成
+    if(Math.floor(Math.random() * 100) === 0) {
+        createCactus();
+    }
+    if(Math.floor(Math.random() * 200) === 0) {
+        createBird();
+    }
 
     // キャクター移動
     movedino(); // 恐竜の移動
+    moveEnemys(); // 敵キャラクターの移動
 
     //描画
     drawdino();// 恐竜の描画
+    drawEnemys(); // 敵キャラクターの描画
 
     // TODO あたり判定
 
@@ -65,6 +73,29 @@ function createDino() {
     }
 }
 
+function createCactus() {
+    game.enemys.push({
+        x: canvas.width + game.image.cactus.width / 2,
+        y: canvas.height - game.image.cactus.height / 2,
+        width: game.image.cactus.width,
+        height: game.image.cactus.height,
+        moveX: -10,
+        image: game.image.cactus
+    });
+}
+
+function createBird() {
+    const birdY = Math.random() * (300 - game.image.bird.height) + 150;
+    game.enemys.push({
+        x: canvas.width + game.image.bird.width / 2,
+        y: birdY,
+        width: game.image.bird.width,
+        height: game.image.bird.height,
+        moveX: -15,
+        image: game.image.bird
+    });
+}
+
 function movedino() {
     game.dino.y += game.dino.moveY;
     if (game.dino.y >= canvas.height - game.dino.height / 2) {
@@ -75,8 +106,20 @@ function movedino() {
     }
 }
 
+function moveEnemys() {
+    for (const enemy of game.enemys) {
+        enemy.x += enemy.moveX;
+    }
+}
+
 function drawdino() {
     ctx.drawImage(game.image.dino, game.dino.x - game.dino.width / 2, game.dino.y - game.dino.height / 2);
+}
+
+function drawEnemys() {
+    for (const enemy of game.enemys) {
+        ctx.drawImage(enemy.image, enemy.x - enemy.width / 2, enemy.y - enemy.height / 2);
+    }
 }
 
 document.onkeydown = function(e) {
